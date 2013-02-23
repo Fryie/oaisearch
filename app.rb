898f20935a490d3ac0980f457fff3b8630813636
Pierpaolo Frasa <pfrasa@smail.uni-koeln.de>
2013-02-23 16:43:55 +0100

    first proof of concept

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'slim'
 
+require 'uri'
+require 'net/http'
+require 'json'
+
 root = File.dirname(File.absolute_path(__FILE__))
 
 get '/' do
@@ -9,11 +13,14 @@ get '/' do
 end
 
 post '/result' do
-  query = '''
-    { "query": {
-        "match": {
-          "dc:description": "'''+params[:search_term]+'''"
-        }
-    }}'''
-  slim :result, locals:{term:query}
+  query = {"query" => { "match" => { "dc:description" => params[:search_term] }}}.to_json
+  
+  uri = URI.parse("http://localhost:9200/ukoeln/_search")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type'=>'application/json'})
+  request.body = query
+  response = http.request(request)
+
+  results = JSON.parse(response.body)["hits"]["hits"]
+  slim :result, locals:{term:results.size}
 end
